cfc08fd1beef9c6c1694ed19bec4f10ed3019289
Rossetag <m.guiking@gmail.com>
2020-07-14 14:11:41 +0200

    made a searchbar

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -2,6 +2,11 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import PreviewCard from '../../components/PreviewCard/PreviewCard'
 import CardColumns from "react-bootstrap/CardColumns";
+import Form from 'react-bootstrap/Form'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function Homepage() {
     const dispatch = useDispatch();
@@ -14,6 +19,20 @@ export default function Homepage() {
 
     return (
         <div>
+            <Jumbotron fluid>
+                <Container>
+                    <Row><Col>
+                        <h1>Most popular parks</h1>
+                    </Col>
+                        <Col>
+                            <Form>
+                                <Form.Control type="text" placeholder="Search" />
+                                <Form.Text className="text-muted">
+                                </Form.Text>
+                            </Form>
+                        </Col> </Row>
+                </Container>
+            </Jumbotron>
             <CardColumns>
                 {/* {parks && parks.map(park => {
                     return (
