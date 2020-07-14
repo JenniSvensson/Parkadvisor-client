28fa67ac9cff2639dc9de689b3620f173b31e59e
Rossetag <m.guiking@gmail.com>
2020-07-14 12:24:24 +0200

    Set up an overview of preview-cards that will be feeded by data from Redux

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Loading from "./components/Loading";
 import MessageBox from "./components/MessageBox";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Homepage from "./pages/Homepage/Homepage"
 
 import { useDispatch, useSelector } from "react-redux";
 import { selectAppLoading } from "./store/appState/selectors";
@@ -38,7 +39,7 @@ function App() {
       <MessageBox />
       {isLoading ? <Loading /> : null}
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Homepage} />
         <Route path="/other" component={Other} />
         <Route path="/signup" component={SignUp} />
         <Route path="/login" component={Login} />
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import Preview from '../components/Preview/Preview'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import PreviewCard from '../../components/PreviewCard/PreviewCard'
+import CardColumns from "react-bootstrap/CardColumns";
 
 export default function Homepage() {
+    const dispatch = useDispatch();
+    // const parks = useSelector(selectParks);
+
+    // useEffect(() => {
+    //     dispatch(fetchParks());
+    // }, [dispatch]);
+
+
     return (
         <div>
-            map over the preview components
+            <CardColumns>
+                {/* {parks && parks.map(park => {
+                    return (
+
+                        <PreviewCard />
+
+                    );
+                })} */}
+
+                <PreviewCard />
+                <PreviewCard />
+                <PreviewCard />
+                <PreviewCard />
+                <PreviewCard />
+                <PreviewCard />
+                <PreviewCard />
+                <PreviewCard />
+
+            </CardColumns>
         </div>
     )
 }
