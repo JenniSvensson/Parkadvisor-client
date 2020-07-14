5f62a7627de1288fde6768d3ab99c2111b3f2a9f
Jenni <jenni.svensson@live.se>
2020-07-14 20:13:56 +0200

    Added a selector to get park by id

--- src/store/parks/selectors.js
+++ src/store/parks/selectors.js
@@ -1,1 +1,11 @@
-export const selectParks = state => state.art;
+export const selectParks = (state) => state.art;
+
+export const selectParkById = (id) => (reduxState) => {
+  if (reduxState.park) {
+    const result = reduxState.park.find((recipe) => {
+      return recipe.id === id;
+    });
+
+    return result;
+  }
+};
