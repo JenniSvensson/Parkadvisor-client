ae3f7fd36ba71b4c96ef6ae4b25cc7fb2c6465c1
Rossetag <m.guiking@gmail.com>
2020-07-14 15:59:30 +0200

    first setup for parkDetails page

--- src/store/parks/actions.js
+++ src/store/parks/actions.js
@@ -1,15 +1,41 @@
 import axios from "axios";
+import { selectUser } from "../user/selectors"
+
 
 export const fetchArtworks = () => {
     return async (dispatch, getState) => {
-        const artCount = getState().art.length;
+        const artCount = getState().park.length;
         const response = await axios.get(`${apiUrl}/parks?limit=10&offset=${artCount}`)
-        console.log("In action: what is my respone?", response.data.artworks.rows);
-        dispatch(fetchParksSuccess(response.data.artworks.rows));
+        console.log("In action: what is my respone?", response.data.parks.rows);
+        dispatch(fetchParksSuccess(response.data.parks.rows));
     }
 }
 
-export const fetchParksSuccess = artworks => ({
+export const fetchParksSuccess = parks => ({
     type: "FETCH_PARKS_SUCCESS",
-    payload: artworks
+    payload: parks
 });
+
+
+export const newReview = (title, rating) => {
+    return async (dispatch, getState) => {
+        const { id, token } = selectUser(getState())
+        //dispatch(appLoading())
+        const response = await axios.post(`${apiUrl}/reviews/new`,
+            {
+                title,
+                rating
+            },
+            {
+                headers: { Authorization: `Bearer ${token}` }
+            }
+        );
+        //dispatch(showMessageWithTimeout("succes", false, response.data.message, 3000));
+        dispatch(reviewPostSucces(response.data.newArtWork))
+    }
+}
+
+export const reviewPostSucces = newReview => ({
+    type: "POST_SUCCES",
+    payload: newReview
+})
