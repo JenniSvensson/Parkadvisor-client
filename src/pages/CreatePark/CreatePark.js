6ffac3a282696c9c1dd9091b1ccf215ac40bb3e1
paula-morales <pmpoclava@gmail.com>
2020-07-14 18:05:03 +0200

    capture a picture with your cam

--- src/pages/CreatePark/CreatePark.js
+++ src/pages/CreatePark/CreatePark.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Form, Button, Container } from "react-bootstrap";
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import { Form, Button, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { selectToken, selectUser } from "../../store/user/selectors";
 import { useHistory } from "react-router-dom";
 import { addPark } from "../../store/parks/actions";
+import Webcam from "react-webcam";
 
 export default function CreatePark() {
   const [title, setTitle] = useState("");
@@ -11,10 +12,16 @@ export default function CreatePark() {
   const [imageUrl, setImageUrl] = useState("");
   const [country, setCountry] = useState("");
   const [type, setType] = useState("");
+  const [toggleCaptureImage, setToggleCaptureImage] = useState(false);
   const dispatch = useDispatch();
-  const user = useSelector(selectUser);
-  //   const token = useSelector(selectToken);
-  //   const history = useHistory();
+  const token = useSelector(selectToken);
+  const history = useHistory();
+  const webcamRef = useRef(null);
+
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImageUrl(imageSrc);
+  }, [webcamRef, setImageUrl]);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -27,18 +34,20 @@ export default function CreatePark() {
     setImageUrl("");
     setCountry("");
     setType("");
+    setToggleCaptureImage(false);
   }
 
-  //   useEffect(() => {
-  //     if (token === null) {
-  //       history.push("/");
-  //     }
-  //   }, [token, history]);
+  useEffect(() => {
+    if (token === null) {
+      history.push("/");
+    }
+  }, [token, history]);
 
   return (
     <div>
       <Container>
         <h1>Add a park</h1>
+
         <Form onSubmit={handleSubmit}>
           <Form.Group controlId="formBasicTitle">
             <Form.Label>Title</Form.Label>
@@ -64,13 +73,45 @@ export default function CreatePark() {
 
           <Form.Group controlId="formBasicImageUrl">
             <Form.Label>Image url</Form.Label>
-            <Form.Control
+            {/* <Form.Control
               name="imageUrl"
               type="text"
               value={imageUrl}
               onChange={(event) => setImageUrl(event.target.value)}
               placeholder="Enter image url"
-            />
+            /> */}
+
+            {toggleCaptureImage ? (
+              <>
+                {" "}
+                <Row>
+                  {!imageUrl ? (
+                    <Webcam
+                      audio={false}
+                      height={200}
+                      ref={webcamRef}
+                      screenshotFormat="image/jpeg"
+                      minScreenshotHeight={200}
+                      minScreenshotWidth={200}
+                    />
+                  ) : (
+                    <img src={imageUrl} />
+                  )}
+                </Row>
+                <Row>
+                  {!imageUrl && (
+                    <Button onClick={capture}>Capture photo</Button>
+                  )}
+                </Row>
+              </>
+            ) : (
+              <Row>
+                {" "}
+                <Button onClick={(e) => setToggleCaptureImage(true)}>
+                  Capture photo
+                </Button>{" "}
+              </Row>
+            )}
           </Form.Group>
 
           <Form.Group controlId="formBasicCountry">
