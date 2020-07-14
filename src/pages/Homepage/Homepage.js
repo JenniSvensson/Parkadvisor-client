b52dcf4805ecec310c93cba59f38ba34516ba97c
Rossetag <m.guiking@gmail.com>
2020-07-14 11:44:20 +0200

    created the basic homepage and preview component

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Preview from '../components/Preview/Preview'
 
 export default function Homepage() {
     return (
         <div>
-
+            map over the preview components
         </div>
     )
 }
