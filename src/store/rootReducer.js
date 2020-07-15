b148a0162be93a085a7d9d565b8bda8ee2856088
Jenni <jenni.svensson@live.se>
2020-07-15 15:47:33 +0200

    Added reviews selector, reducer and action

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import appState from "./appState/reducer";
 import parks from "./parks/reducer";
 import user from "./user/reducer";
+import reviews from "./reviews/reducer";
 
 export default combineReducers({
   appState,
   user,
   parks,
+  reviews,
 });
