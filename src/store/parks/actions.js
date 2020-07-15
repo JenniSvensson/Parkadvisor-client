838d43ac403ffc23fd49b692b6053e66fc5de963
Jenni <jenni.svensson@live.se>
2020-07-15 15:48:35 +0200

    Moved reviews to its own store

--- src/store/parks/actions.js
+++ src/store/parks/actions.js
@@ -3,80 +3,42 @@ import { selectUser } from "../user/selectors";
 import { apiUrl } from "../../config/constants";
 import { setMessage } from "../appState/actions";
 
-export const newReview = (description, name, stars, parkId) => {
-    return async (dispatch, getState) => {
-        const { token } = selectUser(getState())
-        //dispatch(appLoading())
-        const response = await axios.post(`${apiUrl}/review`,
-            {
-                name,
-                description,
-                stars,
-                parkId
-            },
-            {
-                headers: { Authorization: `Bearer ${token}` }
-            }
-        );
-        //dispatch(showMessageWithTimeout("succes", false, response.data.message, 3000));
-        dispatch(reviewPostSucces(response.data.newPark))
-    }
-}
-export const reviewPostSucces = newReview => ({
-    type: "POST_REVIEW_SUCCES",
-    payload: newReview
-})
-//FETCH REVIEWS FOR A PARK
-export const fetchReviews = (parkId) => {
-    return async (dispatch, getState) => {
-        const response = await axios.get(
-            `${apiUrl}/park/${parkId}/reviews`
-        );
-        console.log("Reviews:", response.data);
-        dispatch(fetchReviewsSuccess(response.data));
-    };
-};
-export const fetchReviewsSuccess = reviews => ({
-    type: "FETCH_REVIEWS_SUCCESS",
-    payload: reviews,
-});
-
 //FETCH PARKS
 export const fetchParks = () => {
-    return async (dispatch, getState) => {
-        const response = await axios.get(`${apiUrl}/park`);
-        console.log("In action: what is my respone?", response.data);
-        dispatch(fetchParksSuccess(response.data));
-    };
+  return async (dispatch, getState) => {
+    const response = await axios.get(`${apiUrl}/park`);
+    console.log("In action: what is my respone?", response.data);
+    dispatch(fetchParksSuccess(response.data));
+  };
 };
 export const fetchParksSuccess = (parks) => ({
-    type: "FETCH_PARKS_SUCCESS",
-    payload: parks,
+  type: "FETCH_PARKS_SUCCESS",
+  payload: parks,
 });
 
 //ADD PARK
 
 export function addPark(title, description, imageUrl, country, type, user) {
-    try {
-        return async function thunk(dispatch, getState) {
-            const user = getState().user;
-            const response = await axios.post(
-                // API endpoint:
-                `${apiUrl}/park`,
-                // Data to be sent along:
-                {
-                    title: title,
-                    description: description,
-                    image: imageUrl,
-                    country: country,
-                    type: type,
-                },
-                { headers: { Authorization: `Bearer ${user.token}` } }
-            );
-            console.log("sucess, the data was sent:", response);
-            dispatch(setMessage("success", true, "Park have been created"));
-        };
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    return async function thunk(dispatch, getState) {
+      const user = getState().user;
+      const response = await axios.post(
+        // API endpoint:
+        `${apiUrl}/park`,
+        // Data to be sent along:
+        {
+          title: title,
+          description: description,
+          image: imageUrl,
+          country: country,
+          type: type,
+        },
+        { headers: { Authorization: `Bearer ${user.token}` } }
+      );
+      console.log("sucess, the data was sent:", response);
+      dispatch(setMessage("success", true, "Park have been created"));
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/store/parks/reducer.js
+++ src/store/parks/reducer.js
@@ -1,17 +1,11 @@
-const initialState = []
+const initialState = [];
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case "FETCH_PARKS_SUCCESS":
-            return action.payload
+  switch (action.type) {
+    case "FETCH_PARKS_SUCCESS":
+      return action.payload;
 
-        case "POST_REVIEW_SUCCES":
-            return [...state, { ...action.payload }]
-
-        case "FETCH_REVIEWS_SUCCESS":
-            return action.payload
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
