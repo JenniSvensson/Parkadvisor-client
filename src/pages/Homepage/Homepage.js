2be61b7571e71bcac1f6c6d1ab0b295dfae93b8c
Jenni <jenni.svensson@live.se>
2020-07-15 17:45:00 +0200

    Fixed so  the right things get mapped

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,57 +1,58 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import PreviewCard from '../../components/PreviewCard/PreviewCard'
+import PreviewCard from "../../components/PreviewCard/PreviewCard";
 import CardColumns from "react-bootstrap/CardColumns";
-import Form from 'react-bootstrap/Form'
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Form from "react-bootstrap/Form";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 //Redux
 import { fetchParks } from "../../store/parks/actions";
 import { selectParks } from "../../store/parks/selectors";
 
 export default function Homepage() {
-    const dispatch = useDispatch();
-    const parks = useSelector(selectParks);
+  const dispatch = useDispatch();
+  const parks = useSelector(selectParks);
 
-    useEffect(() => {
-        dispatch(fetchParks());
-        console.log("parks in homepage:", parks)
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchParks());
+    console.log("parks in homepage:", parks);
+  }, [dispatch]);
 
-
-    return (
-        <div>
-            <Jumbotron fluid>
-                <Container>
-                    <Row><Col>
-                        <h1>Most popular parks</h1>
-                    </Col>
-                        <Col>
-                            <Form>
-                                <Form.Control type="text" placeholder="Search" />
-                                <Form.Text className="text-muted">
-                                </Form.Text>
-                            </Form>
-                        </Col> </Row>
-                </Container>
-            </Jumbotron>
-            <CardColumns>
-                {parks && parks.map(park => {
-                    return (
-                        <PreviewCard
-                            title={park.title}
-                            description={park.description}
-                            imageUrl={park.image}
-                            country={park.country}
-                            type={park.type}
-                            id={park.id} />
-                    );
-                })}
-
-            </CardColumns>
-        </div>
-    )
+  return (
+    <div>
+      <Jumbotron fluid>
+        <Container>
+          <Row>
+            <Col>
+              <h1>Most popular parks</h1>
+            </Col>
+            <Col>
+              <Form>
+                <Form.Control type="text" placeholder="Search" />
+                <Form.Text className="text-muted"></Form.Text>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
+      <CardColumns>
+        {parks.rows &&
+          parks.rows.map((park) => {
+            return (
+              <PreviewCard
+                title={park.title}
+                description={park.description}
+                imageUrl={park.image}
+                country={park.country}
+                type={park.type}
+                id={park.id}
+              />
+            );
+          })}
+      </CardColumns>
+    </div>
+  );
 }
