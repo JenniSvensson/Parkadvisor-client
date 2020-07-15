969cc34bad41b6fd15c1c8cad4e731edf3691038
Jenni <jenni.svensson@live.se>
2020-07-15 17:42:46 +0200

    Changed the seletor to correctly fetch the data

--- src/store/parks/selectors.js
+++ src/store/parks/selectors.js
@@ -1,12 +1,10 @@
-
-export const selectParks = state => state.parks;
+export const selectParks = (state) => state.parks;
 
 export const selectParkById = (id) => (state) => {
-  if (state.parks.length) {
-    const result = state.parks.find((park) => {
+  if (state.parks.rows) {
+    const result = state.parks.rows.find((park) => {
       return park.id === parseInt(id);
     });
     return result;
   }
 };
-
