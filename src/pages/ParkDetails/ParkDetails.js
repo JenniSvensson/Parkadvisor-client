1ec226ee5361b5ee56ec064670d8f0d9f8ba03f3
Jenni <jenni.svensson@live.se>
2020-07-16 10:34:47 +0200

    Fixed so that form is only visable signed in. Also displaying stars.

--- src/pages/ParkDetails/ParkDetails.js
+++ src/pages/ParkDetails/ParkDetails.js
@@ -17,21 +17,21 @@ import { selectReviews } from "../../store/reviews/selectors";
 import { CloudinaryContext } from "cloudinary-react";
 import { fetchPhotos, openUploadWidget } from "../../config/CloudinaryService";
 import { showMessageWithTimeout } from "../../store/appState/actions";
+import { selectToken } from "../../store/user/selectors";
 
 export default function ParkDetails() {
   const [reviewText, setReviewText] = useState();
   const [title, setTitle] = useState();
-  const [stars, setStars] = useState(4);
+  const [stars, setStars] = useState();
   const [imageUrl, setImageUrl] = useState("");
-
   const { id } = useParams();
   const dispatch = useDispatch();
+  const token = useSelector(selectToken);
   const reviews = useSelector(selectReviews);
   const currentPark = useSelector(selectParkById(id));
 
   function handleSubmit(e) {
     e.preventDefault();
-
     dispatch(newReview(reviewText, title, stars, id));
     //reset form
     setReviewText("");
@@ -39,14 +39,14 @@ export default function ParkDetails() {
   }
 
   const meanRating = () => {
-    let rating = 0
-    currentPark.reviews.forEach(review => {
-      rating += review.stars
-    })
-    rating = Math.floor(rating / currentPark.reviews.length)
-    const star = "★"
-    return star.repeat(rating)
-  }
+    let rating = 0;
+    currentPark.reviews.forEach((review) => {
+      rating += review.stars;
+    });
+    rating = Math.floor(rating / currentPark.reviews.length);
+    const star = "★";
+    return star.repeat(rating);
+  };
 
   useEffect(() => {
     dispatch(fetchParks());
@@ -82,61 +82,73 @@ export default function ParkDetails() {
             <div>
               <h1>{currentPark.title}</h1>
               {meanRating()}
-              <Image
-                src={`${currentPark.image}`}
-                className="image"
-                fluid />
+              <Image src={`${currentPark.image}`} className="image" fluid />
               <p>{currentPark.description}</p>
             </div>
           ) : (
-              <p>Loading</p>
-            )}
+            <p>Loading</p>
+          )}
         </Row>
         <Row>
-          <Form onSubmit={handleSubmit}>
-            <CloudinaryContext cloudName="parkadvisor">
-              <Form.Group controlId="formBasicTitle">
-                <Form.Label>Title</Form.Label>
-                <Form.Control
-                  onChange={(e) => setTitle(e.target.value)}
-                  type="text"
-                  name="title"
-                  value={title}
-                  placeholder="Enter title"
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicComment">
-                <Form.Label>Leave a review</Form.Label>
-                <Form.Control
-                  onChange={(e) => setReviewText(e.target.value)}
-                  type="text"
-                  name="review-text"
-                  value={reviewText}
-                  placeholder="Enter comment"
-                />
-              </Form.Group>
-              <Form.Group controlId="formBasicImageUrl">
-                <Form.Label>Image (1)</Form.Label>
-                <Row>
-                  <Button onClick={() => beginUpload()}>Upload Image</Button>
-                </Row>
-                {imageUrl && (
-                  <Image
-                    src={imageUrl}
-                    className="img-responsive"
-                    style={{
-                      maxHeight: "25vh",
-                      maxWidth: "35vw",
-                      padding: "10px 0",
-                    }}
+          {token && (
+            <Form onSubmit={handleSubmit}>
+              <CloudinaryContext cloudName="parkadvisor">
+                <Form.Group controlId="formBasicTitle">
+                  <Form.Label>Title</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setTitle(e.target.value)}
+                    type="text"
+                    name="title"
+                    value={title}
+                    placeholder="Enter title"
                   />
-                )}
-              </Form.Group>
-              <Button type="submit" value="submit">
-                Submit
-              </Button>
-            </CloudinaryContext>
-          </Form>
+                </Form.Group>
+
+                <Form.Group
+                  onChange={(e) => setStars(e.target.value)}
+                  controlId="formBasicStars"
+                >
+                  <Form.Label>Add a rating</Form.Label>
+                  <Form.Check name="stars" value="1" type="radio" label="1" />
+                  <Form.Check name="stars" value="2" type="radio" label="2" />
+                  <Form.Check name="stars" value="3" type="radio" label="3" />
+                  <Form.Check name="stars" value="4" type="radio" label="4" />
+                  <Form.Check name="stars" value="5" type="radio" label="5" />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicComment">
+                  <Form.Label>Leave a review</Form.Label>
+                  <Form.Control
+                    onChange={(e) => setReviewText(e.target.value)}
+                    type="text"
+                    name="review-text"
+                    value={reviewText}
+                    placeholder="Enter comment"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formBasicImageUrl">
+                  <Form.Label>Image (1)</Form.Label>
+                  <Row>
+                    <Button onClick={() => beginUpload()}>Upload Image</Button>
+                  </Row>
+                  {imageUrl && (
+                    <Image
+                      src={imageUrl}
+                      className="img-responsive"
+                      style={{
+                        maxHeight: "25vh",
+                        maxWidth: "35vw",
+                        padding: "10px 0",
+                      }}
+                    />
+                  )}
+                </Form.Group>
+                <Button type="submit" value="submit">
+                  Submit
+                </Button>
+              </CloudinaryContext>
+            </Form>
+          )}
         </Row>
         <Col>
           <h1>Reviews({reviews.length})</h1>
@@ -145,6 +157,7 @@ export default function ParkDetails() {
               <div key={review.id}>
                 <h2>{review.name}</h2>
                 <h5>{review.updatedAt}</h5>
+                {"★".repeat(review.stars)}
                 <p>{review.description}</p>
               </div>
             );
