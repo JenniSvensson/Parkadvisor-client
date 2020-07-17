21f89697ec5b552c195ecb0a109eb79cb4fbf73a
paula-morales <pmpoclava@gmail.com>
2020-07-17 16:19:34 +0200

    clean up code

--- src/store/parks/actions.js
+++ src/store/parks/actions.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { selectUser } from "../user/selectors";
 import { apiUrl } from "../../config/constants";
 import { showMessageWithTimeout } from "../appState/actions";
 
@@ -12,7 +11,6 @@ export const fetchParks = () => {
   };
 };
 export const fetchParksSuccess = (parks) => ({
-
   type: "FETCH_PARKS_SUCCESS",
   payload: parks,
 });
@@ -21,7 +19,9 @@ export const fetchParksSuccess = (parks) => ({
 export const fetchMoreParks = () => {
   return async (dispatch, getState) => {
     const offset = getState().parks.length;
-    const response = await axios.get(`${apiUrl}/park?offset=${offset}&limit=10`);
+    const response = await axios.get(
+      `${apiUrl}/park?offset=${offset}&limit=10`
+    );
     //console.log("Fetched more parks:", response.data.rows);
     dispatch(fetchMoreParksSuccess(response.data.rows));
   };
@@ -33,50 +33,38 @@ export const fetchMoreParksSuccess = (parks) => ({
 
 //ADD PARK
 export function addPark(title, description, imageUrl, country, type, user) {
-  try {
-    return async function thunk(dispatch, getState) {
-      const user = getState().user;
-      const response = await axios.post(
-        // API endpoint:
-        `${apiUrl}/park`,
-        // Data to be sent along:
-        {
-          title: title,
-          description: description,
-          image: imageUrl,
-          country: country,
-          type: type,
-        },
-        { headers: { Authorization: `Bearer ${user.token}` } }
-      );
-      console.log("sucess, the data was sent:", response);
+  return async function thunk(dispatch, getState) {
+    const user = getState().user;
+    const response = await axios.post(
+      // API endpoint:
+      `${apiUrl}/park`,
+      // Data to be sent along:
+      {
+        title: title,
+        description: description,
+        image: imageUrl,
+        country: country,
+        type: type,
+      },
+      { headers: { Authorization: `Bearer ${user.token}` } }
+    );
+    console.log("sucess, the data was sent:", response);
 
-      dispatch(
-        showMessageWithTimeout("success", true, "Park have been created")
-      );
-    };
-  } catch (error) {
-    console.log(error);
-  }
+    dispatch(showMessageWithTimeout("success", true, "Park have been created"));
+  };
 }
 
 //REPORT A PARK
 export function reportPark(parkId) {
-  try {
-    return async function thunk(dispatch, getState) {
-      const user = getState().user;
-      const response = await axios.patch(
-        // API endpoint:
-        `${apiUrl}/park/${parkId}/report`,
-        { headers: { Authorization: `Bearer ${user.token}` } }
-      );
-      console.log("reported:", response);
+  return async function thunk(dispatch, getState) {
+    const user = getState().user;
+    const response = await axios.patch(
+      // API endpoint:
+      `${apiUrl}/park/${parkId}/report`,
+      { headers: { Authorization: `Bearer ${user.token}` } }
+    );
+    console.log("reported:", response);
 
-      dispatch(
-        showMessageWithTimeout("success", true, "Park has been reported")
-      );
-    };
-  } catch (error) {
-    console.log(error);
-  }
+    dispatch(showMessageWithTimeout("success", true, "Park has been reported"));
+  };
 }
